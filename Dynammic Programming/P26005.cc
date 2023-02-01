9b954a0d285e1bee752e9d3c34b9baa81efe7563
p4tac0 <104589010+p4tac0@users.noreply.github.com>
2023-02-01 12:56:57 +0100

    Update P26005.cc

--- Dynammic Programming/P26005.cc
+++ Dynammic Programming/P26005.cc
@@ -1,3 +1,10 @@
+/*
+Some problems are so classic that barely need a statement. For this one, please compute the minimum cost to insert letters into two words w1 and w2 to make them identical. Both words are made up of only letters chosen among the n smallest lowercase letters (for instance, for n=4, the alphabet is {a, b, c, d}). For every letter (call it x), inserting an x in any place in any word has cost Ix.
+
+Input consists of several cases. Each case begins with 2≤ n≤ 26, followed by n strictly positive natural numbers Ia, Ib, Ic, …. Follow two words w1 and w2 made up of between 1 and 1000 lowercase letters chosen among the n smallest letters. Assume 1≤ Ix≤ 1000 for every letter x.
+
+For every case, print the minimum cost to make w1 and w2 identical.
+*/
 #include <algorithm>
 #include <iostream>
 #include <vector>
